d704b0ff1af2aead3d87cb61eb87fa7ded6ab9e4
Aaron Pang <pykm05@gmail.com>
2024-09-25 00:47:01 -0700

    create React server instance and listen for GET requests to homepage URL

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,1 @@
-export const PORT = 5555;
+export const PORT = 3000;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 import express from "express";
 import { PORT } from "./config.js"
 
+// create app object/instance of express application
+// instance of imported module that provdies methods for middleware
+// middleware: bridge between os/database and application
 const app = express();
 
+// creates new http route
+// http route: function that runs when a url is visited
+// respond with 'welcome to my world' when a GET request is made to homepage
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome to my world');
+});
+
+// create new server instance and start listening to incoming requests
 app.listen(PORT, () => {
     console.log('App is listening to port: ' + PORT)
 });
