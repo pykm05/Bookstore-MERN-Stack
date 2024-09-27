d4b4c5febf37c49e84d53a45c487e728ab37fee7
Aaron Pang <pykm05@gmail.com>
2024-09-26 23:53:51 -0700

    define new mongo schema

---
+++ backend/models/bookModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+// Mongoose document: wrapper of schema, provides interface to database for creating, querying, ect.
+// Mongoose schema: defines the sturcture of document
+const bookSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        publishYear: {
+            type: String,
+            required: true,
+        },
+    }
+);
+
+export const Book = mongoose.model('Book', bookSchema)
