dcd5cb66e609fee64679f4147fd43b791a84b074
Aaron Pang <pykm05@gmail.com>
2024-10-01 23:18:13 -0700

    change middleware from express.json to cors

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
-import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 // npm run dev
 
@@ -10,6 +10,26 @@ import booksRoute from './routes/booksRoute.js';
 // instance of imported module that provdies methods for middleware
 // middleware: bridge between os/database and application
 const app = express();
+
+// Middleware for handling CORS Policy
+// Option 1: Allow All Origins with Default of cors(*)
+// Allows everything
+
+// Option 2: Allow Custom Origins
+// Specifiy which websites it accepts data from
+
+// CORS defines a way for web apps to interact with resources in different domains
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
+// Middleware for parsing reqeust body
+// Accepts data in json format
+// app.use(express.json());
 
 // Book requests are rerouted to 
 app.use('./books', booksRoute);
