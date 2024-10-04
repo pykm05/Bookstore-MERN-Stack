ab44da9c86c41772068edc83f28b5e3e9d771931
Aaron Pang <pykm05@gmail.com>
2024-10-03 23:30:49 -0700

    implemented single page application and React router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
 
+// SPA (Single Page Application)
+// Loads and updates a single HTML page as user interacts with app
+
+// React Router Dom
+// Directs you to certain page depending on request
+// Updates view without reloading page
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    // Define how to navigate thorough different pages in application
+    // Renders componenets based on url
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='./books/edit/:id' element={<EditBook />} />
+      <Route path='./books/delete/:id' element={<DeleteBook />} />
+    </Routes>
   )
 }
 
